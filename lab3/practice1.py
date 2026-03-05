dc9f5984daf53a8b5de057a48d5a8ea3921577a4
hayrunnisaiyikosker <hniyikosker@gmail.com>
2026-03-05 22:05:32 +0200

    lab3: complete practice 1

---
+++ lab3/practice1.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+# TODO: Import BaseModel from pydantic
+
+app = FastAPI()
+
+# Mock database of books
+books = [
+    {"id": 1, "title": "1984", "author": "George Orwell", "year": 1949},
+    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960},
+    {"id": 3, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925}
+]
+
+
+# TODO: Define a Pydantic model for the book
+# Include:
+# - title
+# - author
+# - year
+class books (BaseModel):
+    title: str
+    author: str
+    year: int
+
+# TODO: Define a GET endpoint receiving the id and use the response model
+# Use the URL: /books/{book_id}
+    # Finding the book in books
+@app.get("/books/{book_id}", response_model = books)
+async def read_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
