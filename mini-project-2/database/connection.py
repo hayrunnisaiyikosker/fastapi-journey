fecbfd931fcc6941aba5264c62562c44d4cc76ee
hayrunnisaiyikosker <hniyikosker@gmail.com>
2026-03-28 15:43:59 +0200

    mini-project-2: implement database connection and Database class

---
+++ mini-project-2/database/connection.py
@@ -0,0 +1,55 @@
+from beanie import init_beanie, Document, PydanticObjectId
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional, List, Any
+from pydantic_settings import BaseSettings
+from models.events import Event
+from models.users import User
+from pydantic import BaseModel
+
+class Settings(BaseSettings):
+    DATABASE_URL: Optional[str] = None
+    
+    class Config:
+        env_file = ".env"
+
+    async def initialize_database(self):
+        client = AsyncIOMotorClient(self.DATABASE_URL)
+        await init_beanie(
+            database = client.get_default_database(),
+            document_models = [Event, User]
+        )
+
+class Database:
+    def __init__(self, model):
+        self.model = model
+    
+    async def save(self, document) -> None:
+        await document.create()
+        return
+
+    async def get(self, id: PydanticObjectId) -> Any:
+        doc = await self.model.get(id)
+        if doc:
+            return doc
+        return False
+
+    async def get_all(self) -> List[Any]:
+        docs = await self.model.find_all().to_list()
+        return docs
+
+    async def update(self, id: PydanticObjectId, body: BaseModel) -> Any:
+        doc = await self.get(id)
+        if not doc:
+            return False
+        des_body = body.dict()
+        des_body = {k: v for k, v in des_body.items() if v is not None}
+        update_query = {"$set": {field: value for field, value in des_body.items()}}
+        await doc.update(update_query)
+        return doc
+    
+    async def delete(self, id: PydanticObjectId) -> bool:
+        doc =await self.get(id)
+        if not doc:
+            return False
+        await doc.delete()
+        return True
